99536d42786400068b205f198b4f114b1e225d1a
Liquan Yang <yang.liquan87@gmail.com>
2022-10-16 20:55:46 -0400

    finish contract + add tests

--- contracts/RoyalPets.sol
+++ contracts/RoyalPets.sol
@@ -5,8 +5,13 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RoyalPets is ERC721URIStorage, ERC2981 {
+contract RoyalPets is ERC721URIStorage, ERC2981, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
   constructor() ERC721("RoyalPets", "RP") {
     // 100 basis points = 1% of sale
     _setDefaultRoyalty(msg.sender, 100);
@@ -29,5 +34,31 @@ contract RoyalPets is ERC721URIStorage, ERC2981 {
 
   function burnNFT(uint256 tokenId) public {
     _burn(tokenId);
+  }
+
+  function mintNFT(address recipient, string memory tokenURI)
+    public
+    onlyOwner
+    returns (uint256)
+  {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _safeMint(recipient, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    return newItemId;
+  }
+
+  function mintNFTWithRoyalty(
+    address recipient,
+    string memory tokenURI,
+    address royaltyReceiver,
+    uint96 feeNumerator
+  ) public onlyOwner returns (uint256) {
+    uint256 tokenId = mintNFT(recipient, tokenURI);
+    _setTokenRoyalty(tokenId, royaltyReceiver, feeNumerator);
+
+    return tokenId;
   }
 }
--- test/test_royal_pets.js
+++ test/test_royal_pets.js
@@ -1,13 +1,69 @@
-const TestRoyalPets = artifacts.require("TestRoyalPets");
+const RoyalPets = artifacts.require("RoyalPets");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("TestRoyalPets", function (/* accounts */) {
-  it("should assert true", async function () {
-    await TestRoyalPets.deployed();
-    return assert.isTrue(true);
+contract("RoyalPets", function(accounts) {
+  it("should support the ERC721 and ERC2198 standards", async () => {
+    const royalPetsInstance = await RoyalPets.deployed();
+    const ERC721InterfaceId = "0x80ac58cd";
+    const ERC2981InterfaceId = "0x2a55205a";
+    const isERC721 = await royalPetsInstance.supportsInterface(
+      ERC721InterfaceId
+    );
+    const isERC2981 = await royalPetsInstance.supportsInterface(
+      ERC2981InterfaceId
+    );
+    assert.equal(isERC721, true, "RoyalPets is not an ERC721");
+    assert.equal(isERC2981, true, "RoyalPets is not an ERC2981");
+  });
+
+  it("should return the correct royalty info when specified and burned", async () => {
+    const royalPetsInstance = await RoyalPets.deployed();
+    // 1st token uses default minting method with default royalty fee
+    await royalPetsInstance.mintNFT(accounts[0], "fakeURI");
+    // 2nd token uses minting that sends royalties to another account with custom royalty fee
+    await royalPetsInstance.mintNFTWithRoyalty(
+      accounts[0],
+      "fakeURI",
+      accounts[1],
+      1000
+    );
+
+    // get royalty info for 1st token
+    const defaultRoyaltyInfo = await royalPetsInstance.royaltyInfo.call(
+      1,
+      1000
+    );
+    // get royalty info for 2nd token
+    let tokenRoyaltyInfo = await royalPetsInstance.royaltyInfo.call(2, 1000);
+    const owner = await royalPetsInstance.owner.call();
+
+    assert.equal(
+      defaultRoyaltyInfo[0],
+      owner,
+      "Default receiver is not the owner"
+    );
+    // Default royalty percentage taken should be 1%.
+    assert.equal(defaultRoyaltyInfo[1].toNumber(), 10, "Royalty fee is not 10");
+
+    assert.equal(
+      tokenRoyaltyInfo[0],
+      accounts[1],
+      "Royalty receiver is not a different account"
+    );
+    // Custom royalty percentage taken should be 10%.
+    assert.equal(tokenRoyaltyInfo[1].toNumber(), 100, "Royalty fee is not 100");
+
+    // Royalty info should be set back to default when NFT is burned
+    await royalPetsInstance.burnNFT(2);
+    tokenRoyaltyInfo = await royalPetsInstance.royaltyInfo.call(2, 1000);
+    assert.equal(
+      tokenRoyaltyInfo[0],
+      owner,
+      "Royalty receiver has not been set back to default"
+    );
+    assert.equal(
+      tokenRoyaltyInfo[1].toNumber(),
+      10,
+      "Royalty has not been set back to default"
+    );
   });
 });
